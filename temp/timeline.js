2af33ba15595eae6ee7158f7de11fb88ab452642
RulerChen <a20196780@gmail.com>
2024-09-28 23:02:03 +0800

    feat: add temp

---
+++ temp/timeline.js
@@ -0,0 +1,74 @@
+export const yahoo = [
+  {
+    title: '1/31 投遞履歷'
+  },
+  {
+    title: '2/22 收到拒絕信'
+  }
+];
+
+export const hp = [
+  {
+    title: '3/3 投遞履歷'
+  }
+];
+
+export const kk = [
+  {
+    title: '3/3 投遞履歷'
+  },
+  {
+    title: '3/8 收到前測'
+  }
+];
+
+export const appier_summer = [
+  {
+    title: '3/3 投遞履歷'
+  }
+];
+
+export const shopback = [
+  {
+    title: '3/3 投遞履歷'
+  },
+  {
+    title: '3/4 收到前測'
+  },
+  {
+    title: '3/15 拒絕'
+  }
+];
+
+export const microsoft = [
+  {
+    title: '3/3 投遞履歷'
+  }
+];
+
+export const dell = [
+  {
+    title: '3/3 投遞履歷'
+  }
+];
+
+export const Line = [
+  {
+    title: '3/3 投遞履歷'
+  }
+];
+
+export const shopee = [
+  {
+    title: '3/3 投遞履歷'
+  },
+  {
+    title: '4/2 收到前測'
+  }
+];
+
+export const trend = [
+  {
+    title: '3/10 投遞履歷'
+  }
+];
