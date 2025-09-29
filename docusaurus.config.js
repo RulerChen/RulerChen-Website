375b1af4f23caf56e984fee806846561990da1b2
RulerChen <a20196780@gmail.com>
2025-09-29 10:06:06 +0800

    migrate config to esm

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,172 +1,212 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+import { themes as prismThemes } from "prism-react-renderer";
 
-const { themes } = require('prism-react-renderer');
-const lightTheme = themes.github;
-const darkTheme = themes.dracula;
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
-/** @type {import('@docusaurus/types').Config} */
-module.exports = async function createConfigAsync() {
-  return {
-    title: 'RulerChen',
-    tagline: 'RulerChen Site',
-    favicon: '/img/profile/profile.webp',
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
-    // Set the production url of your site here
-    url: 'https://RulerChen.github.io',
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/RulerChen-Website',
+const config = {
+  title: "RulerChen",
+  tagline: "RulerChen Site",
+  favicon: "/img/profile/profile.webp",
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'RulerChen', // Usually your GitHub org/user name.
-    projectName: 'RulerChen-Website', // Usually your repo name.
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true // Improve compatibility with the upcoming Docusaurus v4
+  },
 
-    onBrokenLinks: 'warn',
+  // Set the production url of your site here
+  url: "https://RulerChen.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/RulerChen-Website",
 
-    markdown: {
-      hooks: {
-        onBrokenMarkdownLinks: 'warn',
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "RulerChen", // Usually your GitHub org/user name.
+  projectName: "RulerChen-Website", // Usually your repo name.
+
+  trailingSlash: true, // Whether to add a trailing slash to the end of URLs.
+
+  onBrokenLinks: "throw",
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "throw"
+    }
+  },
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "zh-Hant",
+    locales: ["zh-Hant", "en"],
+    localeConfigs: {
+      "zh-Hant": {
+        label: "繁體中文",
+        direction: "ltr",
+        htmlLang: "zh-Hant"
       },
-    },
+      en: {
+        label: "English",
+        direction: "ltr",
+        htmlLang: "en"
+      }
+    }
+  },
 
-    // Even if you don't use internalization, you can use this field to set useful
-    // metadata like html lang. For example, if your site is Chinese, you may want
-    // to replace "en" with "zh-Hans".
-    i18n: {
-      defaultLocale: 'zh-Hant',
-      locales: ['zh-Hant', 'en'],
-      localeConfigs: {
-        'zh-Hant': {
-          label: '繁體中文',
-          direction: 'ltr',
-          htmlLang: 'zh-Hant',
-        },
-        en: {
-          label: 'English',
-          direction: 'ltr',
-          htmlLang: 'en',
-        },
-      },
-    },
+  headTags: [
+    {
+      tagName: "link",
+      attributes: {
+        rel: "preconnect",
+        href: "https://cdn.jsdelivr.net",
+        crossorigin: "anonymous"
+      }
+    }
+  ],
 
-    trailingSlash: true,
-
-    future: {
-      v4: true,
-      experimental_faster: true,
-    },
-
-    headTags: [
+  presets: [
+    [
+      "classic",
       {
-        tagName: 'meta',
-        attributes: {
-          name: 'google-site-verification',
-          content: 'QrdRbPEcOsJJ_kfRVewqfwR6GjPcRf0UVgRb85I-5fc',
+        docs: {
+          sidebarPath: "./sidebars.js",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/RulerChen/RulerChen-Website/tree/main/",
+          showLastUpdateTime: true,
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex]
         },
-      },
-      {
-        tagName: 'link',
-        attributes: {
-          rel: 'preconnect',
-          href: 'https://cdn.jsdelivr.net',
-          crossorigin: 'anonymous',
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/RulerChen/RulerChen-Website/tree/main/",
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn"
         },
-      },
-    ],
-
-    presets: [
-      [
-        'classic',
-        /** @type {import('@docusaurus/preset-classic').Options} */
-        ({
-          docs: {
-            sidebarPath: require.resolve('./sidebars.js'),
-            editUrl: 'https://github.com/RulerChen/RulerChen-Website/tree/main/',
-            showLastUpdateTime: true,
-            remarkPlugins: [(await import('remark-math')).default],
-            rehypePlugins: [(await import('rehype-katex')).default],
-          },
-          theme: {
-            customCss: require.resolve('./src/css/custom.css'),
-          },
-          gtag: {
-            trackingID: 'G-8BEHDPLQMG',
-            anonymizeIP: true,
-          },
-          sitemap: {
-            changefreq: 'daily',
-            priority: 0.5,
-            ignorePatterns: [
-              '/RulerChen-Website/blog/archive/',
-              '/RulerChen-Website/search/',
-              '/RulerChen-Website/docs/intro/',
-              '/RulerChen-Website/blog/',
-            ],
-            filename: 'sitemap.xml',
-          },
-        }),
-      ],
-    ],
-    stylesheets: [
-      {
-        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-        type: 'text/css',
-        integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-        crossorigin: 'anonymous',
-      },
-    ],
-    themeConfig:
-      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-      ({
-        algolia: {
-          apiKey: '2b8f51df143e1ab1b63c05ce84dd422a',
-          indexName: 'RulerChen-Website',
-          appId: 'GSV9DQB5UM',
+        theme: {
+          customCss: "./src/css/custom.css"
         },
-        metadata: [
-          {
-            name: 'description',
-            content: '這是一個關於資訊領域的個人筆記網站，包含了前後端技術，資料結構與演算法等等的相關知識',
-          },
-        ],
-        // Replace with your project's social card
-        image: 'img/profile/profile.webp',
-        navbar: {
-          title: 'RulerChen',
-          logo: {
-            alt: 'My Site Logo',
-            src: 'img/profile/profile.webp',
-          },
-
-          items: [
-            {
-              type: 'docSidebar',
-              sidebarId: 'tutorialSidebar',
-              position: 'left',
-              label: 'Notes',
-            },
-            { to: 'blog', label: 'Blog', position: 'left' },
-            {
-              type: 'localeDropdown',
-              position: 'right',
-            },
+        gtag: {
+          trackingID: "G-8BEHDPLQMG",
+          anonymizeIP: true
+        },
+        sitemap: {
+          changefreq: "daily",
+          priority: 0.5,
+          ignorePatterns: [
+            "/RulerChen-Website/blog/archive/",
+            "/RulerChen-Website/search/",
+            "/RulerChen-Website/docs/intro/",
+            "/RulerChen-Website/blog/"
           ],
+          filename: "sitemap.xml"
+        }
+      }
+    ]
+  ],
+
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity: "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous"
+    }
+  ],
+  
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/profile/profile.webp",
+    algolia: {
+      apiKey: "2b8f51df143e1ab1b63c05ce84dd422a",
+      indexName: "RulerChen-Website",
+      appId: "GSV9DQB5UM"
+    },
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: true
+    },
+    navbar: {
+      title: "RulerChen",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/profile/profile.webp"
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Notes"
         },
-        colorMode: {
-          defaultMode: 'dark',
-          disableSwitch: true,
-        },
-        // footer: {
-        //   style: 'dark',
-        //   copyright: `Copyright © ${new Date().getFullYear()} RulerChen, Inc. Built with Docusaurus.`
-        // },
-        prism: {
-          additionalLanguages: ['powershell'],
-          theme: lightTheme,
-          darkTheme: darkTheme,
-        },
-      }),
-  };
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          type: "localeDropdown",
+          position: "right"
+        }
+      ]
+    },
+    // footer: {
+    //   style: "dark",
+    //   links: [
+    //     {
+    //       title: "Docs",
+    //       items: [
+    //         {
+    //           label: "Tutorial",
+    //           to: "/docs/intro"
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       title: "Community",
+    //       items: [
+    //         {
+    //           label: "Stack Overflow",
+    //           href: "https://stackoverflow.com/questions/tagged/docusaurus"
+    //         },
+    //         {
+    //           label: "Discord",
+    //           href: "https://discordapp.com/invite/docusaurus"
+    //         },
+    //         {
+    //           label: "X",
+    //           href: "https://x.com/docusaurus"
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       title: "More",
+    //       items: [
+    //         {
+    //           label: "Blog",
+    //           to: "/blog"
+    //         },
+    //         {
+    //           label: "GitHub",
+    //           href: "https://github.com/facebook/docusaurus"
+    //         }
+    //       ]
+    //     }
+    //   ],
+    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+    // },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula
+    }
+  }
 };
+
+export default config;
