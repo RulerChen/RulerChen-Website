a6d0bec80fc18bcf42a8d4c75ca1d79c2882a301
RulerChen <a20196780@gmail.com>
2025-10-13 19:39:37 +0800

    fix: correct label from 'Notes' to 'Note' in sidebar configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -172,7 +172,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Notes',
+          label: 'Note',
         },
         { to: 'blog', label: 'Blog', position: 'left' },
         {
